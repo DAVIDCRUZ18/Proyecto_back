ac3c4e068298035616a40ac9c48d166ff02cf304
David cruz martinez <lx12466@innovaccion.mx>
2023-06-12 00:39:10 -0500

    ganeracion del proyecto y sus categorias

---
+++ ecommerceTest/Producto/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Producto,Categoria,Carrito,Pedido,ItemCarrito,Usuario,Comentario,Favorito,Direccion,Venta
+# Register your models here.
+
+admin.site.register(Producto)
+admin.site.register(Categoria)
+admin.site.register(Carrito)
+admin.site.register(ItemCarrito)
+admin.site.register(Pedido)
+admin.site.register(Usuario)
+admin.site.register(Comentario)
+admin.site.register(Favorito)
+admin.site.register(Direccion)
+admin.site.register(Venta)
---
+++ ecommerceTest/Producto/models.py
@@ -0,0 +1,102 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen = models.ImageField(upload_to='productos/')
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class Categoria(models.Model):
+    Nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.Nombre
+
+class Carrito(models.Model):
+    usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    productos = models.ManyToManyField('Producto', through='ItemCarrito')
+
+    def __str__(self):
+        return f"Carrito de {self.usuario}"
+
+
+class ItemCarrito(models.Model):
+    carrito = models.ForeignKey('Carrito', on_delete=models.CASCADE)
+    producto = models.ForeignKey('Producto', on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.producto} en {self.carrito}"
+
+class Pedido(models.Model):
+    carrito = models.OneToOneField('Carrito', on_delete=models.CASCADE)
+    fecha_pedido = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Pedido #{self.id}"
+
+
+class Usuario(AbstractUser):
+    nombre_tienda = models.CharField(max_length=255)
+    direccion_envio = models.CharField(max_length=255)
+    numero_telefono = models.CharField(max_length=20)
+    
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='usuarios',
+        blank=True,
+        help_text='The groups this user belongs to.'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='usuarios',
+        blank=True,
+        help_text='Specific permissions for this user.'
+    )
+
+    def __str__(self):
+        return self.username
+
+
+class Comentario(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    contenido = models.TextField()
+    calificacion = models.IntegerField()
+
+    def __str__(self):
+        return f"Comentario de {self.usuario} en {self.producto}"
+
+class Favorito(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.usuario} ha marcado como favorito {self.producto}"
+
+class Direccion(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    direccion = models.CharField(max_length=255)
+    ciudad = models.CharField(max_length=100)
+    codigo_postal = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"Dirección de {self.usuario}: {self.direccion}, {self.ciudad}"
+
+class Venta(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_venta = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Venta de {self.producto} ({self.cantidad} unidades)"
+
